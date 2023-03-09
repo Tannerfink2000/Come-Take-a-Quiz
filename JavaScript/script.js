5154e4ed42f987df6ffed12029e6107bdd294810
Tanner Fink <tannerfink2000@gmail.com>
2023-03-08 17:33:02 -0800

    added css

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,20 @@
+  var startButton = document.getElementById("start");
+  var quizContainer = document.getElementById("quiz-container");
+  var questionElement = document.getElementById("question");
+  var choicesElement = document.getElementById("choices");
+  var resultContainer = document.getElementById("result-container");
+  var resultText = document.getElementById("result-text");
+  var scoreContainer = document.getElementById("score-container");
+  var scoreElement = document.getElementById("score");
+  var submitContainer = document.getElementById("submit-container");
+  var initialsForm = document.getElementById("initials-form");
+  var initialsInput = document.getElementById("initials");
+  var submitButton = document.getElementById("submit");
+  
+  var currentQuestion = 0;
+  var score = 0;
+  var timeLeft = 60;
+  var timerInterval;
 
   function start() {
     window.location.href = "question.html";
