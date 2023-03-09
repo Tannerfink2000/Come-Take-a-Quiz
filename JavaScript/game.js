3edc07d0f86396440c8f5b73c8becc78c144e495
Tanner Fink <tannerfink2000@gmail.com>
2023-03-09 15:33:43 -0800

    added more js

--- JavaScript/game.js
+++ JavaScript/game.js
@@ -1,25 +1,91 @@
 var question = document.querySelector('#progressText')
 var scoreText = document.querySelector('#score')
 var choices = document.querySelectorAll('.choice-text')
+var timerEl = document.querySelector('#timer');
+
 var questions = [
     {
-        question: "what is an object?",
-        answers:["1", "2","4","3"],
-        correct:"1"
+        question: "Which keyword is used to declare a variable in JavaScript?",
+        answers:["A) let", "B) declare","C) var","D) set"],
+        correct:"A) let"
     },
     {
-        question: "what is an object",
-        answers:["2", "1","4","3"],
-        correct:"1"
-    }
+        question: "How do you add a comment in JavaScript code?",
+        answers:["A) // comment", "B) <!-- comment -->","C) /* comment */","D) all of the above"],
+        correct:"A) // comment"
+    },
+    {
+        question: "Which of the following is NOT a JavaScript data type?",
+        answers:["A) string", "B) boolean","C) integer","D) array"],
+        correct:"C) integer"
+    },
+    {
+        question: "How do you declare a function in JavaScript?",
+        answers:["A) function myFunction()", "B) myFunction()","C) def myFunction()","D) declare myFunction()"],
+        correct:"A) function myFunction()"
+    },
+    {
+        question: "Which built-in JavaScript function is used to convert a string to a number?",
+        answers:["A) parseInt()", "B) toString()","C) parseFloat()","D) toNumber()"],
+        correct:"A) parseInt()"
+    },
+    
 ]
-function updateQuestion(potato) {
-    question.textContent=questions[potato].question
-    for(var i = 0;i<choices.length; i++) {
-        choices[i].textContent=questions[potato].answers[i]
-    }
+var currentQuestion = 0;
+var score = 0;
+var timeLeft = 60;
+var timerInterval;
 
+function startTimer() {
+    timerInterval = setInterval(function() {
+        timeLeft--;
+        timerEl.textContent = "Time left: " + timeLeft;
+
+        if (timeLeft <= 0) {
+            clearInterval(timerInterval);
+            endQuiz();
+        }
+    }, 1000);
 }
 
-updateQuestion(1)
+function endQuiz() {
+    clearInterval(timerInterval);
+    localStorage.setItem("mostRecentScore", score);
+    window.location.href = "results.html";
+}
+
+function updateQuestion() {
+    question.textContent = questions[currentQuestion].question;
+    for (var i = 0; i < choices.length; i++) {
+        choices[i].textContent = questions[currentQuestion].answers[i];
+    }
+}
+
+function checkAnswer(e) {
+    var selectedChoice = e.target;
+    var selectedAnswer = selectedChoice.textContent;
+    var correctAnswer = questions[currentQuestion].correct;
+    
+    if (selectedAnswer === correctAnswer) {
+        score++;
+    }
+
+    currentQuestion++;
+
+    if (currentQuestion < questions.length) {
+        updateQuestion();
+    } else {
+        var finalScore = score * 100;
+        localStorage.setItem("score", finalScore);
+        window.location.href = "./results.html";
+    }
+}
+
+updateQuestion();
+startTimer();
+
+for (var i = 0; i < choices.length; i++) {
+    choices[i].addEventListener("click", checkAnswer);
+}
+
 
